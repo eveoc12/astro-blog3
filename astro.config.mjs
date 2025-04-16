36bf0aa7e68c8d05c37186fda36e47a4f0d3e46d
eveoc12 <1043544606@qq.com>
2025-04-16 10:48:11 +0800

    UI 框架：Preact 交互式的问候组件。何时可能不选择岛屿进行交互

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site : "https://eve-first-astrovlog.netlify.app/"
+  site : "https://eve-first-astrovlog.netlify.app/",
+  integrations: [preact()]
 });
